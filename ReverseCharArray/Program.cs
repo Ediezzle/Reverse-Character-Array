e6f364a990b5389f36ce4349911c75ca4821c563
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-29 19:43:54 +0200

    once-off commit

---
+++ ReverseCharArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseCharArray
+{
+    class Program
+    {
+		public static char[] Reverse(char[] array)
+		{
+			string s = "";
+			for (int i = array.Length - 1; i >= 0; i--)
+			{
+				s += array[i];
+
+			}
+			char[] charArray = s.ToCharArray();
+			return charArray;
+		}
+		static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			char[] cA = { 'a', 'b', 'c', 'd' };
+			Console.WriteLine(Reverse(cA));
+		}
+    }
+}
